2f440cb70eab3b8f1409c08f17e9ebc77ec65d58
Sayo <82053242+wtfsayo@users.noreply.github.com>
2024-11-08 00:28:37 +0700

    update buttons and search icon

--- components/searchInput.tsx
+++ components/searchInput.tsx
@@ -5,6 +5,7 @@ import handleSearch from "@/actions/handleSearch";
 import Loading from "@/components/loading";
 import { Suspense } from "react";
 import { Analytics } from "@vercel/analytics/react";
+import { SearchIcon } from "lucide-react";
 
 interface SearchInputProps {
   orgs: string[];
@@ -35,20 +36,26 @@ export default function SearchInput({ orgs }: SearchInputProps) {
         <h1 className="text-3xl font-serif font-bold text-gray-200 mb-8 text-center">
           GrantScan
         </h1>
-        <form action={handleSearch} className="max-w-3xl mx-auto">
+        <form action={handleSearch} className="max-w-3xl mx-auto relative">
           <input
             type="text"
             name="query"
             placeholder="Search Projects"
             defaultValue={query === "all" ? "" : query}
-            className="w-full bg-gray-900 border border-gray-700 rounded-lg px-6 py-3 text-gray-300 focus:outline-none focus:border-gray-600 text-lg"
+            className="w-full bg-gray-900 border border-gray-700 rounded-lg px-6 py-3 text-gray-300 focus:outline-none focus:border-gray-600 text-lg pr-12"
           />
+          <button
+            type="submit"
+            className="absolute right-2 top-1/2 -translate-y-1/2 p-2 text-gray-400 hover:text-gray-200 transition-colors"
+          >
+            <SearchIcon size={20} />
+          </button>
         </form>
         <Suspense fallback={<Loading />}>
           <div className="flex gap-2 mt-4 max-w-3xl mx-auto overflow-x-auto py-2">
             <button
               onClick={() => handleOrgClick("All")}
-              className={`px-4 py-1.5 rounded-full text-sm transition-colors ${
+              className={`w-max whitespace-nowrap rounded-full py-2 px-4 h-full text-sm transition-colors ${
                 !selectedOrg
                   ? "bg-gray-800 text-white"
                   : "bg-gray-900 text-gray-400 hover:bg-gray-800"
@@ -60,7 +67,7 @@ export default function SearchInput({ orgs }: SearchInputProps) {
               <button
                 key={index}
                 onClick={() => handleOrgClick(org)}
-                className={`px-4 py-1.5 rounded-full text-sm transition-colors ${
+                className={`w-max whitespace-nowrap rounded-full py-2 px-4 h-full text-sm transition-colors ${
                   selectedOrg === org
                     ? "bg-gray-800 text-white"
                     : "bg-gray-900 text-gray-400 hover:bg-gray-800"
