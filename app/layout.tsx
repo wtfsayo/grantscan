7329ad272004106e3b6f35d7051325a1e40eb3ef
Sayo <82053242+wtfsayo@users.noreply.github.com>
2024-12-11 14:15:30 +0530

    test frames v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
+import Provider from "@/components/WagmiProvider";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Analytics />
-        {children}
+        <Provider>
+          <Analytics />
+          {children}
+        </Provider>
       </body>
     </html>
   );
