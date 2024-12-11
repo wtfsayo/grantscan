716f26e02987e04b70b01e5c4764ecff2a1d9ee0
Sayo <82053242+wtfsayo@users.noreply.github.com>
2024-12-11 15:31:19 +0530

    test frames

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import type { Metadata } from "next";
 import { Providers } from "@/components/providers";
 
 const geistSans = localFont({
@@ -14,34 +13,6 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
-
-const frame = {
-  version: "next",
-  imageUrl: `https://grantscan.org/og.png`,
-  button: {
-    title: "Open Frame",
-    action: {
-      type: "launch_frame",
-      name: "GrantScan | Indexing all grant applications ever ",
-      url: `https://grantscan.org/search/all`,
-      splashImageUrl: `https://grantscan.org/splash.png`,
-      splashBackgroundColor: "#020202",
-    },
-  },
-};
-
-export const metadata: Metadata = {
-  title: "GrantScan",
-  description: "Indexing all grant applications ever",
-  openGraph: {
-    title: "GrantScan",
-    description: "Indexing all grant applications ever",
-    images: [`https://grantscan.org/og.png`],
-  },
-  other: {
-    "fc:frame": JSON.stringify(frame),
-  },
-};
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,37 @@
 import { redirect } from "next/navigation";
+import { Metadata } from "next";
 
+const frame = {
+  version: "next",
+  imageUrl: `https://grantscan.org/og.png`,
+  button: {
+    title: "Open Frame",
+    action: {
+      type: "launch_frame",
+      name: "GrantScan | Indexing all grant applications ever ",
+      url: `https://grantscan.org/search/all`,
+      splashImageUrl: `https://grantscan.org/splash.png`,
+      splashBackgroundColor: "#020202",
+    },
+  },
+};
+
+export const revalidate = 300;
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "GrantScan",
+    description: "Indexing all grant applications ever",
+    openGraph: {
+      title: "GrantScan",
+      description: "Indexing all grant applications ever",
+      images: [`https://grantscan.org/og.png`],
+    },
+    other: {
+      "fc:frame": JSON.stringify(frame),
+    },
+  };
+}
 export default function Home() {
   redirect("/search/all");
 }
